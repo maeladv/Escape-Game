d5e3c2443519b299565b01dc47c5afb9a2be1a79
“Maeladv” <advissemael@gmail.com>
2025-06-14 20:44:30 +0200

    feat: il est maintenant possible d'ajouter un item a un objet. Si l'objet possede un item parmi ses varibles, le joueur ne pourra pas interagir avec l'objet si il n'a pas l'item dans son iventaire. Il est toujours possible de définir les objets sans item et dans ce cas, on interagit avec l'objet comme avant

--- App/Objets/Objet.java
+++ App/Objets/Objet.java
@@ -1,21 +1,39 @@
 package App.Objets;
 import java.awt.Rectangle;
 
+import App.Inventaire.Item;
+import App.Inventaire.Inventaire;
+
 public class Objet {
     private String name; // Nom de l'objet
     private Rectangle hitbox;
     private Runnable onCollision; // Action à exécuter lors de la collision
     private boolean alreadyTriggered = false;
     private boolean isActive = false; // Indique si l'objet est actif (joueur regarde dans sa direction)
+    private Inventaire inventaire;
+    private Item itemToInteract; // Item associé à cet objet, peut être null si pas d'interaction
 
-    public Objet(String name, Rectangle hitbox, Runnable onCollision) {
+    public Objet(String name, Rectangle hitbox, Inventaire inventaire, Runnable onCollision) {
         this.name = name;
         this.hitbox = hitbox;
+        this.inventaire = inventaire;
+        this.itemToInteract = null;
+        this.onCollision = onCollision;
+    }
+
+    public Objet(String name, Rectangle hitbox, Inventaire inventaire, Item itemToInteract, Runnable onCollision) {
+        this.name = name;
+        this.hitbox = hitbox;
+        this.inventaire = inventaire;
+        this.itemToInteract = itemToInteract;
         this.onCollision = onCollision;
     }
 
     public void trigger() {
-        if (!alreadyTriggered && onCollision != null) {
+        if (!alreadyTriggered && onCollision != null && itemToInteract != null && inventaire.contientItem(itemToInteract.getName())) {
+            onCollision.run();
+            alreadyTriggered = true;
+        } else if (!alreadyTriggered && onCollision != null && itemToInteract == null) {
             onCollision.run();
             alreadyTriggered = true;
         }
